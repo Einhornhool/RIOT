dd2b0f88b085dd6f9a26357346cd905d83fe698b
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2019-07-23 14:08:20 +0200

    change checksum back to old status

--- tests/saml11/main.c
+++ tests/saml11/main.c
@@ -5,9 +5,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-/*Default Adresse um 1 geschoben, um rw-Bit zu ignorieren (0xC0 >> 1)*/
+/* Default Adresse um 1 geschoben, um rw-Bit zu ignorieren (0xC0 >> 1) */
 #define DEV_ADR (0x60)
-/*Word Address -> auszulesender Datenbereich*/
+/* Word Address -> auszulesender Datenbereich */
 #define READ_REG (0x03)
 
 #define POLYNOM (0x8005)
@@ -17,54 +17,55 @@
 
 void i2c_wakeup(void)
 {
-    //SDA als GPIO, Output
+    /* SDA als GPIO, Output */
     gpio_init(GPIO_PIN(0, 16), GPIO_OUT);
     gpio_clear(GPIO_PIN(0, 16));
 
-    //60 us warten (t(WLO))
+    /* 0 us warten (t(WLO)) */
     xtimer_usleep(30);
 
-    //i2c-Device reinitialisieren
+    /* i2c-Device reinitialisieren */
     i2c_init(I2C_DEV(0));
 
-    //1500 us warten (t(WHI))
+    /* 1500 us warten (t(WHI)) */
     xtimer_usleep(1500);
 }
 
-typedef struct __attribute__((__packed__)) Content
-{
-    uint8_t opcode;
-    uint8_t param1;
-    uint16_t param2;
-} Content_t;
+// typedef struct __attribute__((__packed__)) Content
+// {
+//     uint8_t opcode;
+//     uint8_t param1;
+//     uint16_t param2;
+// } Content_t;
 
 typedef struct __attribute__((__packed__)) Packet
 {
     uint8_t count;
-    Content_t content;
+    uint8_t opcode;
+    uint8_t param1;
+    uint16_t param2;
     uint16_t checksum;
 } Packet_t;
 
 int main (void)
 {
-    Content_t cont;
     Packet_t infoCmd;
-    cont.opcode = CMD_INFO;
-    cont.param1 = 0;
-    cont.param2 = 0;
-
-    infoCmd.content = cont;
-    infoCmd.checksum = ucrc16_calc_le((const uint8_t*) &cont, sizeof(cont), POLYNOM, 0);
     infoCmd.count = sizeof(infoCmd);
+    infoCmd.opcode = CMD_INFO;
+    infoCmd.param1 = 0;
+    infoCmd.param2 = 0;
+
+    /* sizeof(infoCmd) minus checksum-bytes */
+    infoCmd.checksum = ucrc16_calc_le((const uint8_t*) &infoCmd, sizeof(infoCmd)-2, POLYNOM, 0);
 
     i2c_wakeup();
     i2c_write_bytes(I2C_DEV(0), DEV_ADR, &infoCmd, sizeof(infoCmd), 0);
-    //0,1 ms warten (t(WLO))
+    /* 0,1 ms warten (t(WLO)) */
     xtimer_usleep(100);
 
     i2c_read_bytes(I2C_DEV(0), DEV_ADR, &infoCmd, sizeof(infoCmd), 0);
     // uint32_t data = 0;
-    // //Daten auslesen
-    // i2c_read_regs(I2C_DEV(0), DEV_ADR, READ_REG, &data, 4, 0);
+    /* Daten auslesen */
+    // i2c_read_regs(I2C_DEV(0), DEV_ADR, READ_REG, &infoCmd, 4, 0);
     return 0;
 }
