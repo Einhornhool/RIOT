d9cf7e516b8584599238f96b9d2c8110f8089650
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2019-08-22 13:31:01 +0200

    add crc-check

--- tests/crc-check/main.c
+++ tests/crc-check/main.c
@@ -1,7 +1,8 @@
-#include "../../sys/include/checksum/ucrc16.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <assert.h>
+
+#include "checksum/ucrc16.h"
 
 #define UINT8_BIT_SIZE              (8U)
 #define UINT16_BIT_SIZE             (16U)
@@ -34,48 +35,15 @@ uint16_t atCRC(size_t length, const uint8_t *data)
     return crc_register;
 }
 
-uint16_t ucrc16_calc_be(const uint8_t *buf, size_t len, uint16_t poly,
-                        uint16_t seed)
-{
-    assert(buf != NULL);
-    for (unsigned c = 0; c < len; c++, buf++) {
-        uint32_t tmp = seed ^ (*buf << (UINT16_BIT_SIZE - UINT8_BIT_SIZE));
-        for (unsigned i = 0; i < UINT8_BIT_SIZE; i++) {
-            tmp = LEFTMOST_BIT_SET(tmp) ? ((tmp << 1) ^ poly) : (tmp << 1);
-        }
-        seed = tmp & UINT16_MAX;
-    }
-    return seed;
-}
-
-uint16_t ucrc16_calc_le(const uint8_t *buf, size_t len, uint16_t poly,
-                        uint16_t seed)
-{
-    assert(buf != NULL);
-    for (unsigned c = 0; c < len; c++, buf++) {
-        seed ^= (*buf);
-        for (unsigned i = 0; i < UINT8_BIT_SIZE; i++) {
-            seed = RIGHTMOST_BIT_SET(seed) ? ((seed >> 1) ^ poly) : (seed >> 1);
-        }
-    }
-
-    uint16_t seedRev;
-    for (int i = 0; i < 16; i++)
-    {
-        seedRev |= ((seed>>i) & 0x01) << 15-i;
-    }
-    return seedRev;
-}
 
 int main(void)
 {
     uint8_t buf[] = { 0x41, 0xcc };
-    uint8_t bufRev[] = { 0x82, 0x33};
-    // uint8_t buf[] = { 0xab, 0xcd };
-    // char buf[] = "123456";
+    uint8_t bufRev[] = { 0x82, 0x33 };
     uint16_t resultAtCRC = atCRC(sizeof(buf), buf);
 
     uint16_t resultUCRCLe = ucrc16_calc_le(buf, sizeof(buf), 0xa001, 0x0000);
+
     uint16_t resultUCRCBe = ucrc16_calc_be(bufRev, sizeof(bufRev), 0x8005, 0x0000);
 
     printf("atCRC: %x\nuCRC-le: %x\nuCRC-be: %x\n", resultAtCRC, resultUCRCLe, resultUCRCBe );
