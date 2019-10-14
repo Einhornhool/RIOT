302e9426faefedc26d193fbff72d954ff93b1470
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2019-10-14 16:45:17 +0200

    info works, add sha command

--- tests/pkg_cryptoauthlib/main.c
+++ tests/pkg_cryptoauthlib/main.c
@@ -13,6 +13,8 @@
 
 #include "xtimer.h"
 
+#define SHA256_HASH_SIZE (32)
+
 int main(void)
 {
     ATCA_STATUS status;
@@ -27,16 +29,37 @@ int main(void)
                 .rx_retries             = 20
     };
 
-    uint8_t revision[4];
+    // uint8_t revision[4] = { 0x00, 0x00, 0x00, 0x00 };
     
-    status = atcab_init(&cfg);
-    printf("%x\n", status);
-    status = atcab_info(revision);
-    printf("%x\n", status);
+    uint8_t teststring[] = "chili cheese fries";
+    uint8_t hashTest[] = {0x36, 0x46, 0xEF, 0xD6, 0x27, 0x6C, 0x0D, 0xCB, 0x4B, 0x07, 0x73, 0x41, 0x88, 0xF4, 
+                            0x17, 0xB4, 0x38, 0xAA, 0xCF, 0xC6, 0xAE, 0xEF, 0xFA, 0xBE, 0xF3, 0xA8, 0x5D, 0x67, 0x42, 0x0D, 0xFE, 0xE5};
 
-    // uint8_t serial[4];
-    // atcab_read_serial_number(serial);
-    printf("%d %d %d %d\n", revision[0], revision[1], revision[2], revision[3]);
+    uint8_t result[SHA256_HASH_SIZE+3];
+    memset(result, 0, SHA256_HASH_SIZE+3); // alles in result auf 0 setzen
+
+    uint16_t testStringSize = sizeof(teststring) -1;
+
+    status = atcab_init(&cfg);
+    printf("Init: %x\n", status);
+    // status = atcab_info(revision);
+    // printf("Execution: %x\n", status);
+
+    // // uint8_t serial[4];
+    // // atcab_read_serial_number(serial);
+    // printf("%d %d %d %d\n", revision[0], revision[1], revision[2], revision[3]);
+    status = atcab_sha_start();
+    status = atcab_sha_end(result, testStringSize, teststring);
+
+    if (memcmp(hashTest, result+1, SHA256_HASH_SIZE) == 0)
+    {
+        printf("Success\n");
+    }
+    else
+    {
+        printf("Not a success\n");
+    }
+    
 
     return 0;
 }
