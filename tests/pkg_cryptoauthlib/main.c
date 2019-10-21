0324422de6fc1721e32bd93d10aa4877d12520eb
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2019-10-21 15:27:40 +0200

    change receive function to polling

--- tests/pkg_cryptoauthlib/main.c
+++ tests/pkg_cryptoauthlib/main.c
@@ -35,31 +35,27 @@ int main(void)
     uint8_t hashTest[] = {0x36, 0x46, 0xEF, 0xD6, 0x27, 0x6C, 0x0D, 0xCB, 0x4B, 0x07, 0x73, 0x41, 0x88, 0xF4, 
                             0x17, 0xB4, 0x38, 0xAA, 0xCF, 0xC6, 0xAE, 0xEF, 0xFA, 0xBE, 0xF3, 0xA8, 0x5D, 0x67, 0x42, 0x0D, 0xFE, 0xE5};
 
-    uint8_t result[SHA256_HASH_SIZE+3];
-    memset(result, 0, SHA256_HASH_SIZE+3); // alles in result auf 0 setzen
+    uint8_t result[SHA256_HASH_SIZE]; // +3 to fit 1 byte length and 2 bytes checksum
+    memset(result, 0, SHA256_HASH_SIZE); // alles in result auf 0 setzen
 
-    uint16_t testStringSize = sizeof(teststring) -1;
-
+    uint16_t testStringSize = (sizeof(teststring)-1); // -1 to ignore \0
+    
     status = atcab_init(&cfg);
-    printf("Init: %x\n", status);
+    
     // status = atcab_info(revision);
     // printf("Execution: %x\n", status);
-
-    // uint8_t serial[4];
-    // atcab_read_serial_number(serial);
     
     status = atcab_sha_start();
     status = atcab_sha_end(result, testStringSize, teststring);
 
-    if (memcmp(hashTest, result+1, SHA256_HASH_SIZE) == 0)
+    if (memcmp(hashTest, result, SHA256_HASH_SIZE) == 0)
     {
         printf("Success\n");
     }
     else
     {
-        printf("Not a success\n");
+        printf("Not a success: Error %x\n", status);
     }
-    printf("Execution: %x\n", status);
 
     return 0;
 }
