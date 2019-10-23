f5974430758211b6b75cb4b137428aa87515f11d
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2019-10-23 18:07:14 +0200

    beauty corrections

--- pkg/cryptoauthlib/contrib/cryptoauthlib_contrib.c
+++ pkg/cryptoauthlib/contrib/cryptoauthlib_contrib.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
-#include "../include/cryptoauthlib_contrib.h"
+#include "cryptoauthlib_contrib.h"
 #include "xtimer.h"
 
 #include "errno.h"
@@ -27,10 +27,9 @@ void atca_delay_ms(uint32_t delay)
     xtimer_usleep(delay * 1000);
 }
 
-
 ATCA_STATUS hal_i2c_init(void *hal, ATCAIfaceCfg *cfg)
 {
-    i2c_init(DEVICE);
+    i2c_init(I2C_DEVICE);
     return ATCA_SUCCESS;
 }
 
@@ -41,15 +40,15 @@ ATCA_STATUS hal_i2c_post_init(ATCAIface iface)
 
 ATCA_STATUS hal_i2c_send(ATCAIface iface, uint8_t *txdata, int txlength)
 {
-    int ret = -1; // return value from riot functions
+    int ret = -1; /* return value from riot functions */
     
-    // First byte in command packages is reserved for HAL layer use as needed
-    // We use it for the word address value 0x03
+    /* First byte in command packages is reserved for HAL layer use as needed
+    We use it for the word address value 0x03 */
     txdata[0] = 0x03;
 
-    // reserved byte isn't included in txlength, yet, so we add 1
+    /* reserved byte isn't included in txlength, yet, so we add 1 */
     int txlength_updated = txlength + 1;
-    ret = i2c_write_bytes(DEVICE, DEV_ADR, txdata, txlength_updated, 0); 
+    ret = i2c_write_bytes(I2C_DEVICE, DEV_ADR, txdata, txlength_updated, 0); 
     
     if (ret != 0)
     {
@@ -62,39 +61,39 @@ ATCA_STATUS hal_i2c_send(ATCAIface iface, uint8_t *txdata, int txlength)
 ATCA_STATUS hal_i2c_receive(ATCAIface iface, uint8_t *rxdata, uint16_t *rxlength)
 {
     uint8_t retries = iface->mIfaceCFG->rx_retries;
-    int ret = -1; // return value of riot functions
-    uint8_t lengthPackage[1] = { 0 };
+    int ret = -1; /* return value of riot functions */
+    uint8_t length_package[1] = { 0 };
 
-    // read first byte (size of output-data) and store it in lengthPackage
-    // to check if it fits into rxdata
+    /* read first byte (size of output-data) and store it in length_package
+    to check if output will fit into rxdata */
     while (retries-- > 0 && ret != 0)
     {
-        ret = i2c_read_byte(DEVICE, DEV_ADR, lengthPackage, 0);
+        ret = i2c_read_byte(I2C_DEVICE, DEV_ADR, length_package, 0);
     }
     if (ret != 0)
     {
         return ATCA_RX_TIMEOUT;
     }
 
-    uint8_t bytesToRead = lengthPackage[0]-1;
+    uint8_t bytes_to_read = length_package[0]-1;
 
-    if (bytesToRead > *rxlength)
+    if (bytes_to_read > *rxlength)
     {
         return ATCA_SMALL_BUFFER;
     }
 
-    // insert lengthPackage into rxdata as first byte (first byte of output data is always the count)
-    // if we don't do this crc will be wrong 
-    rxdata[0] = lengthPackage[0];
+    /* insert length_package into rxdata as first byte (first byte of output 
+    data is always the count) if we don't do this crc will be wrong */
+    rxdata[0] = length_package[0];
 
-    // reset ret and retries to read the rest of the output
+    /* reset ret and retries to read the rest of the output */
     ret = -1;
     retries = iface->mIfaceCFG->rx_retries;
 
-    // read rest of output and insert into data array after first byte
+    /* read rest of output and insert into rxdata array after first byte */
     while (retries-- > 0 && ret != 0)
     {
-        ret = i2c_read_bytes(DEVICE, DEV_ADR, rxdata+1, bytesToRead, 0);
+        ret = i2c_read_bytes(I2C_DEVICE, DEV_ADR, (rxdata + 1), bytes_to_read, 0);
     }
 
     if (ret != 0)
@@ -102,26 +101,26 @@ ATCA_STATUS hal_i2c_receive(ATCAIface iface, uint8_t *rxdata, uint16_t *rxlength
         return ATCA_RX_TIMEOUT;
     }
 
-    *rxlength = lengthPackage[0];
+    *rxlength = length_package[0];
 
     return ATCA_SUCCESS;
 }
 
 ATCA_STATUS hal_i2c_wake(ATCAIface iface)
 {
-    // Device needs to be woken up by holding sda pin low for some time and then reinitializing it
+    /* I2C_DEVICE needs to be woken up by holding sda pin low for some time and then reinitializing it */
     
-    // SDA as GPIO, Output to manually set it to low
+    /* SDA as GPIO, Output to manually set it to low */
     gpio_init(GPIO_PIN(0, 16), GPIO_OUT);
     gpio_clear(GPIO_PIN(0, 16));
 
-    // wait 30 us (t(WLO))
+    /* wait 30 us (t(WLO)) */
     xtimer_usleep(30);
 
-    // reinitialize i2c-Device
+    /* reinitialize i2c-I2C_DEVICE */
     i2c_init(I2C_DEV(0));
 
-    // wait 1500 us (t(WHI))
+    /* wait 1500 us (t(WHI)) */
     xtimer_usleep(1500);
 
     return ATCA_SUCCESS;
@@ -129,25 +128,25 @@ ATCA_STATUS hal_i2c_wake(ATCAIface iface)
 
 ATCA_STATUS hal_i2c_idle(ATCAIface iface)
 {
-    // idle state = write byte to register adr. 0x02
+    /* idle state = write byte to register adr. 0x02 */
     uint8_t idle[1] = { 0x01 }; 
-    i2c_write_regs(DEVICE, DEV_ADR, IDLE_ADR, idle, 1, 0);
+    i2c_write_regs(I2C_DEVICE, DEV_ADR, IDLE_ADR, idle, 1, 0);
 
     return ATCA_SUCCESS;
 }
 
 ATCA_STATUS hal_i2c_sleep(ATCAIface iface)
 {
-    // sleep state = write byte to register adr. 0x01
+    /* sleep state = write byte to register adr. 0x01 */
     uint8_t sleep[1] = { 0x01 }; 
-    i2c_write_regs(DEVICE, DEV_ADR, SLEEP_ADR, sleep, 1, 0);
+    i2c_write_regs(I2C_DEVICE, DEV_ADR, SLEEP_ADR, sleep, 1, 0);
 
     return ATCA_SUCCESS;
 }
 
 ATCA_STATUS hal_i2c_release(void *hal_data)
 {
-    if(i2c_release(DEVICE) == -1)
+    if(i2c_release(I2C_DEVICE) == -1)
     {
         return ATCA_COMM_FAIL;
     }
@@ -159,7 +158,7 @@ ATCA_STATUS hal_i2c_discover_buses(int i2c_buses[], int max_buses)
     return ATCA_UNIMPLEMENTED;
 }
 
-ATCA_STATUS hal_i2c_discover_devices(int bus_num, ATCAIfaceCfg *cfg, int *found)
+ATCA_STATUS hal_i2c_discover_I2C_DEVICEs(int bus_num, ATCAIfaceCfg *cfg, int *found)
 {
     return ATCA_UNIMPLEMENTED;
 }
--- tests/pkg_cryptoauthlib/main.c
+++ tests/pkg_cryptoauthlib/main.c
@@ -29,31 +29,36 @@ int main(void)
                 .rx_retries             = 20
     };
     
-    uint8_t teststring[] = "chili cheese fries";
-    uint8_t hashTest[] = {0x36, 0x46, 0xEF, 0xD6, 0x27, 0x6C, 0x0D, 0xCB, 0x4B, 0x07, 0x73, 0x41, 0x88, 0xF4, 
-                            0x17, 0xB4, 0x38, 0xAA, 0xCF, 0xC6, 0xAE, 0xEF, 0xFA, 0xBE, 0xF3, 0xA8, 0x5D, 0x67, 0x42, 0x0D, 0xFE, 0xE5};
+    // uint8_t revision[4] = { 0x00, 0x00, 0x00, 0x00 };
+
+    // uint8_t teststring[] = "chili cheese fries";
+    // uint8_t hash_test[] = {0x36, 0x46, 0xEF, 0xD6, 0x27, 0x6C, 0x0D, 0xCB, 0x4B, 0x07, 0x73, 0x41, 0x88, 0xF4, 
+                            // 0x17, 0xB4, 0x38, 0xAA, 0xCF, 0xC6, 0xAE, 0xEF, 0xFA, 0xBE, 0xF3, 0xA8, 0x5D, 0x67, 0x42, 0x0D, 0xFE, 0xE5};
 
     uint8_t result[SHA256_HASH_SIZE]; // +3 to fit 1 byte length and 2 bytes checksum
     memset(result, 0, SHA256_HASH_SIZE); // alles in result auf 0 setzen
 
-    uint16_t testStringSize = (sizeof(teststring)-1); // -1 to ignore \0
+    // uint16_t test_string_size = (sizeof(teststring)-1); // -1 to ignore \0
     
-    status = atcab_init(&cfg);    
-    status = atcab_sha_start();
-    status = atcab_sha_end(result, testStringSize, teststring);
+    status = atcab_init(&cfg);
 
-    // status = atcab_random(result);
-    // printf("Status: %x\n", status);
-    // printf("Number: %x %x %x %x %x %x %x %x\n", 
-    // result[0], result[1], result[2],result[3], result[4],result[5],result[6],result[7]);
+    status = atcab_random(result);
+    printf("Status: %x\n", status);
+    printf("Number: %x %x %x %x %x %x %x %x\n", 
+    result[0], result[1], result[2],result[3], result[4],result[5],result[6],result[7]);
+    // status = atcab_info(revision);
+    // printf("info status= %x\n", status);
+    // printf("Revision= %x %x %x %x\n", revision[0], revision[1], revision[2], revision[3]);
 
-    if (memcmp(hashTest, result, SHA256_HASH_SIZE) == 0)
-    {
-        printf("Success\n");
-    }
-    else
-    {
-        printf("Not a success. Status: %x\n", status);
+    // status = atcab_sha_start();
+    // status = atcab_sha_end(result, test_string_size, teststring);
+    // if (memcmp(hash_test, result, SHA256_HASH_SIZE) == 0)
+    // {
+    //     printf("Success\n");
+    // }
+    // else
+    // {
+    //     printf("Not a success. Status: %x\n", status);
     // }
 
     return 0;
