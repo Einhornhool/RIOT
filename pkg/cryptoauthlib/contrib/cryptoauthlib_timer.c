ebe4cded8adc0a68017ab0f2b165e3e05b46c6a5
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2019-10-25 18:35:34 +0200

    add params and headerfiles

---
+++ pkg/cryptoauthlib/contrib/cryptoauthlib_timer.c
@@ -0,0 +1,19 @@
+#include "cryptoauthlib.h"
+#include "hal/atca_hal.h"
+
+#include "xtimer.h"
+
+void atca_delay_us(uint32_t delay)
+{
+    xtimer_usleep(delay);
+}
+
+void atca_delay_10us(uint32_t delay)
+{
+    xtimer_usleep(delay * 10);
+}
+
+void atca_delay_ms(uint32_t delay)
+{
+    xtimer_usleep(delay * 1000);
+}
