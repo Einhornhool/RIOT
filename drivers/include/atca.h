30bdba81b3922884d5092ee4bade3568cf436387
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2019-12-02 13:09:51 +0100

    add atca.h to drivers/include

---
+++ drivers/include/atca.h
@@ -0,0 +1,77 @@
+/**
+* @defgroup     
+* @ingroup      pkg
+* @brief
+* @{
+*
+* @file
+* @brief
+*
+* More detailed information about the file and the functionality implemented.
+*
+* @author      
+*
+*/
+
+#ifndef CRYPTOAUTHLIB_CONTRIB_H
+#define CRYPTOAUTHLIB_CONTRIB_H
+
+#include "periph/i2c.h"
+#include "cryptoauthlib.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define BUS                         (dev->p.i2c) /**< BUS */
+#define ADDR                        (dev->p.addr) /**< ADDR */
+
+/**
+ * @brief   ATCA specific return values
+ */
+enum {
+    ATCA_OK,          /**< Success, no error */
+    ATCA_ERROR_BUS,   /**< I2C bus error */
+    ATCA_ERROR_DEV,   /**< internal device error */
+    ATCA_ERROR_CONF,  /**< invalid device configuration */
+    ATCA_ERROR,       /**< general error */
+};
+
+/* For ATECC508A*/
+#ifndef ATCA_I2C_ADR
+#define ATCA_I2C_ADR (0x60)    /**< Default device adress is 0xC0. We need to shift it by 1, to ignore lsb (rw bit) */
+#endif
+
+#ifndef ATCA_DATA_ADR
+#define ATCA_DATA_ADR (0x03)            /**< Word Address to read data area */
+#endif
+
+#define ATCA_SLEEP_ADR (0x01)           /**< Address to write byte to enter sleep mode */
+#define ATCA_IDLE_ADR (0x02)            /**< Address to write byte to enter idle mode */
+
+// /**
+//  * @brief   ATCA configuration parameters
+//  */
+// typedef struct {
+//     i2c_t i2c;              /**< I2C bus the sensor is connected to */
+//     uint8_t addr;           /**< the chip's address on the I2C bus */
+// } atca_params_t;
+
+/**
+ * @brief   Device descriptor contains ATCAIfaceCfg structure
+ */
+typedef struct {
+    ATCAIfaceCfg params;        /**< device configuration */
+} atca_t;
+
+/**
+ * @brief   ATCA device init
+ */
+int atca_init(atca_t *dev, const ATCAIfaceCfg *params);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CRYPTOAUTHLIB_CONTRIB_H */
+/** @} */
