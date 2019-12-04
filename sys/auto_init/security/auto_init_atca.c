60c54c5b53e6689b74e459d624c826233f1bd515
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2019-12-04 16:03:21 +0100

    use atcab_init() to initialize devices

--- sys/auto_init/security/auto_init_atca.c
+++ sys/auto_init/security/auto_init_atca.c
@@ -17,17 +17,17 @@
 #define ENABLE_DEBUG                (0)
 #include "debug.h"
 
-#define ATCA_NUMOF (ARRAY_SIZE(atca_params))
-
-/**
- * @brief   Allocate memory for the device descriptors
- */
-static atca_t atca_devs[ATCA_NUMOF];
-
-void auto_init_atca(void) {
+void auto_init_atca(void) {        
         for (unsigned i = 0; i < ATCA_NUMOF; i++)
         {
-                hal_i2c_init(((void*) &atca_devs[i]), ((ATCAIfaceCfg*) &atca_params[i]));
+                LOG_DEBUG("[auto_init_security] initializing atca device #%u\n", i);
+                
+                ATCAIfaceCfg cfg = atca_params[i];
+                if (atcab_init(&cfg) != ATCA_SUCCESS)
+                {
+                        LOG_ERROR("[auto_init_security] error initializing atca device #%u\n", i);
+                        continue;
+                }
         }
 }
 #else
