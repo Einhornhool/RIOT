083db0b911f126ba874ec400e7638e74d121d93a
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2020-07-30 17:42:39 +0200

    examples/hello-world: build nrf-sdk

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -23,18 +23,12 @@
 #include <stdint.h>
 #include <string.h>
 
-#ifdef NRF52840_XXAA
-// #include "vendor/nrf52840.h"
+#include "vendor/nrf52840.h"
 #include "sdk_common.h"
 #include "nrf_crypto.h"
 #include "nrf_crypto_error.h"
 #include "nrf_crypto_hash.h"
 #include "mem_manager.h"
-#endif
-
-#ifdef ARM_CRYPTOCELL
-#include "armcc_setup.h"
-#endif
 
 #include "hashes/sha1.h"
 #include "hashes/sha256.h"
@@ -70,198 +64,105 @@ static uint8_t TEST_0_ENC[] = {
 /* Timer variables */
 uint32_t start, stop, t_diff;
 
-#ifdef NRF52840_XXAA
-    static void nrf_sdk_sha256(void)
-    {
-        nrf_crypto_hash_context_t ctx;
-        nrf_crypto_hash_sha256_digest_t result;
-        size_t result_len = NRF_CRYPTO_HASH_SIZE_SHA256;
-
-        start = xtimer_now_usec();
-        if (nrf_crypto_hash_init(&ctx, &g_nrf_crypto_hash_sha256_info)) {
-            printf("CC310 Sha256 Init failed\n");
-        }
-        if (nrf_crypto_hash_update(&ctx, (uint8_t*) teststring, teststring_size)) {
-            printf("CC310 Sha256 Update failed\n");
-        }
-        if (nrf_crypto_hash_finalize(&ctx, result, &result_len)) {
-            printf("CC310 Sha256 finish failed\n");
-        }
-        stop = xtimer_now_usec();
-        t_diff = stop - start;
-        printf("CC310 Sha256 Time: %ld us\n", t_diff);
-
-        if (memcmp((uint8_t*)result, expected_result_sha256, SHA256_DIGEST_LENGTH) != 0) {
-                printf("CC310 SHA-256 Failure\n");
-
-                for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-                    printf("%02x ", sha256_result[i]);
-                }
-                printf("\n");
-        }
-        else {
-            printf("CC310 SHA-256 Success\n");
-        }
-    }
-
-    static void nrf_sdk_aes(void)
-    {
-        nrf_crypto_aes_context_t enc, dec;
-
-        uint8_t data[AES_BLOCK_SIZE];
-        size_t data_size = 16;
-
-        if (nrf_crypto_aes_init(&enc, &g_nrf_crypto_aes_ecb_128_info, NRF_CRYPTO_ENCRYPT)) {
-            printf("CC310 AES encrypt init failed\n");
-        }
-        if (nrf_crypto_aes_init(&dec, &g_nrf_crypto_aes_ecb_128_info, NRF_CRYPTO_DECRYPT)) {
-            printf("CC310 AES decrypt init failed\n");
-        }
-
-        /* Encryption */
-        start = xtimer_now_usec();
-        if (nrf_crypto_aes_key_set(&enc, TEST_0_KEY)) {
-            printf("CC310 AES set encrypt key failed\n");
-        }
-        stop = xtimer_now_usec();
-        t_diff = stop - start;
-        printf("CC310 AES set encrypt key time: %ld us\n", t_diff);
-
-        start = xtimer_now_usec();
-        if (nrf_crypto_aes_finalize(&enc, TEST_0_INP, sizeof(TEST_0_INP), data, &data_size)) {
-            printf("CC310 AES encrypt failed\n");
-        }
-        stop = xtimer_now_usec();
-        t_diff = stop - start;
-        printf("CC310 AES encryption time: %ld us\n", t_diff);
-        if (!memcmp(data, TEST_0_ENC, AES_BLOCK_SIZE)) {
-            printf("CC310 AES encryption successful\n");
-        }
-        else {
-            printf("CC310 AES encryption failed\n");
-            for (int i = 0; i < 16; i++) {
-                printf("%02x ", data[i]);
-            }
-            printf("\n");
-        }
-
-        /* Decryption */
-        start = xtimer_now_usec();
-        if (nrf_crypto_aes_key_set(&dec, TEST_0_KEY)) {
-            printf("CC310 AES set decrypt key failed\n");
-        }
-        stop = xtimer_now_usec();
-        t_diff = stop - start;
-        printf("CC310 AES set decrypt key time: %ld us\n", t_diff);
-
-        start = xtimer_now_usec();
-        if (nrf_crypto_aes_finalize(&dec, TEST_0_ENC, sizeof(TEST_0_ENC), data, &data_size)) {
-            printf("CC310 AES decrypt failed\n");
-        }
-        stop = xtimer_now_usec();
-        t_diff = stop - start;
-        printf("CC310 AES decryption time: %ld us\n", t_diff);
-        if (!memcmp(data, TEST_0_INP, AES_BLOCK_SIZE)) {
-            printf("CC310 AES decryption successful\n");
-        }
-        else {
-            printf("CC310 AES decryption failed\n");
-            for (int i = 0; i < 16; i++) {
-                printf("%02x ", data[i]);
-            }
-            printf("\n");
-        }
-    }
-#endif
-
-static void sha1_test(void)
+static void nrf_sdk_sha256(void)
 {
+    nrf_crypto_hash_context_t ctx;
+    nrf_crypto_hash_sha256_digest_t result;
+    size_t result_len = NRF_CRYPTO_HASH_SIZE_SHA256;
+
     start = xtimer_now_usec();
-    sha1(sha1_result, (unsigned char*)teststring, teststring_size);
+    if (nrf_crypto_hash_init(&ctx, &g_nrf_crypto_hash_sha256_info)) {
+        printf("CC310 Sha256 Init failed\n");
+    }
+    if (nrf_crypto_hash_update(&ctx, (uint8_t*) teststring, teststring_size)) {
+        printf("CC310 Sha256 Update failed\n");
+    }
+    if (nrf_crypto_hash_finalize(&ctx, result, &result_len)) {
+        printf("CC310 Sha256 finish failed\n");
+    }
     stop = xtimer_now_usec();
     t_diff = stop - start;
-    printf("Sha1 Time: %ld us\n", t_diff);
+    printf("CC310 Sha256 Time: %ld us\n", t_diff);
 
-    if (memcmp(sha1_result, expected_result_sha1, SHA1_DIGEST_LENGTH) != 0) {
-        printf("SHA-1 Failure\n");
+    if (memcmp((uint8_t*)result, expected_result_sha256, SHA256_DIGEST_LENGTH) != 0) {
+            printf("CC310 SHA-256 Failure\n");
 
-        for (int i = 0; i < SHA1_DIGEST_LENGTH; i++) {
-            printf("%02x ", sha1_result[i]);
-        }
-        printf("\n");
+            for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+                printf("%02x ", sha256_result[i]);
+            }
+            printf("\n");
     }
     else {
-        printf("SHA-1 Success\n");
+        printf("CC310 SHA-256 Success\n");
     }
 }
 
-static void sha256_test(void)
+static void nrf_sdk_aes(void)
 {
-    start = xtimer_now_usec();
-    sha256((unsigned char*)teststring, teststring_size, sha256_result);
-    stop = xtimer_now_usec();
-    t_diff = stop - start;
-    printf("Sha256 Time: %ld us\n", t_diff);
+    nrf_crypto_aes_context_t enc, dec;
 
-    if (memcmp(sha256_result, expected_result_sha256, SHA256_DIGEST_LENGTH) != 0) {
-        printf("SHA-256 Failure\n");
-
-        for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-            printf("%02x ", sha256_result[i]);
-        }
-        printf("\n");
-    }
-    else {
-        printf("SHA-256 Success\n");
-    }
-}
-
-static void aes_test(void)
-{
-    int err;
-    cipher_context_t c_ctx;
     uint8_t data[AES_BLOCK_SIZE];
-    memset(data, 0, AES_BLOCK_SIZE);
+    size_t data_size = 16;
 
-    err = aes_init(&c_ctx, TEST_0_KEY, AES_KEY_SIZE);
-    if (err < 1) {
-        printf("AES Init failed: %d\n", err);
+    if (nrf_crypto_aes_init(&enc, &g_nrf_crypto_aes_ecb_128_info, NRF_CRYPTO_ENCRYPT)) {
+        printf("CC310 AES encrypt init failed\n");
+    }
+    if (nrf_crypto_aes_init(&dec, &g_nrf_crypto_aes_ecb_128_info, NRF_CRYPTO_DECRYPT)) {
+        printf("CC310 AES decrypt init failed\n");
     }
 
+    /* Encryption */
     start = xtimer_now_usec();
-    if (aes_encrypt(&c_ctx, TEST_0_INP, data)) {
-        stop = xtimer_now_usec();
-        t_diff = stop - start;
-        printf("AES Encrypt time: %ld us\n", t_diff);
-        if (!memcmp(data, TEST_0_ENC, AES_BLOCK_SIZE)) {
-            printf("AES encryption successful\n");
-        }
-        else
-        {
-            printf("AES encryption failed\n");
-            for (int i = 0; i < 16; i++) {
-                printf("%02x ", data[i]);
-            }
-            printf("\n");
-        }
+    if (nrf_crypto_aes_key_set(&enc, TEST_0_KEY)) {
+        printf("CC310 AES set encrypt key failed\n");
     }
-    memset(data, 0, AES_BLOCK_SIZE);
+    stop = xtimer_now_usec();
+    t_diff = stop - start;
+    printf("CC310 AES set encrypt key time: %ld us\n", t_diff);
+
     start = xtimer_now_usec();
-    if (aes_decrypt(&c_ctx, TEST_0_ENC, data)) {
-        stop = xtimer_now_usec();
-        t_diff = stop - start;
-        printf("AES Decrypt time: %ld us\n", t_diff);
-        if (!memcmp(data, TEST_0_INP, AES_BLOCK_SIZE)) {
-            printf("AES decryption successful\n");
+    if (nrf_crypto_aes_finalize(&enc, TEST_0_INP, sizeof(TEST_0_INP), data, &data_size)) {
+        printf("CC310 AES encrypt failed\n");
+    }
+    stop = xtimer_now_usec();
+    t_diff = stop - start;
+    printf("CC310 AES encryption time: %ld us\n", t_diff);
+    if (!memcmp(data, TEST_0_ENC, AES_BLOCK_SIZE)) {
+        printf("CC310 AES encryption successful\n");
+    }
+    else {
+        printf("CC310 AES encryption failed\n");
+        for (int i = 0; i < 16; i++) {
+            printf("%02x ", data[i]);
         }
-        else
-        {
-            printf("AES decryption failed\n");
-            for (int i = 0; i < 16; i++) {
-                printf("%02x ", data[i]);
-            }
-            printf("\n");
+        printf("\n");
+    }
+
+    /* Decryption */
+    start = xtimer_now_usec();
+    if (nrf_crypto_aes_key_set(&dec, TEST_0_KEY)) {
+        printf("CC310 AES set decrypt key failed\n");
+    }
+    stop = xtimer_now_usec();
+    t_diff = stop - start;
+    printf("CC310 AES set decrypt key time: %ld us\n", t_diff);
+
+    start = xtimer_now_usec();
+    if (nrf_crypto_aes_finalize(&dec, TEST_0_ENC, sizeof(TEST_0_ENC), data, &data_size)) {
+        printf("CC310 AES decrypt failed\n");
+    }
+    stop = xtimer_now_usec();
+    t_diff = stop - start;
+    printf("CC310 AES decryption time: %ld us\n", t_diff);
+    if (!memcmp(data, TEST_0_INP, AES_BLOCK_SIZE)) {
+        printf("CC310 AES decryption successful\n");
+    }
+    else {
+        printf("CC310 AES decryption failed\n");
+        for (int i = 0; i < 16; i++) {
+            printf("%02x ", data[i]);
         }
+        printf("\n");
     }
 }
 
@@ -271,7 +172,6 @@ int main(void)
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
 
-#ifdef NRF52840_XXAA
     // Initialize crypto subsystem
     if (nrf_crypto_init() != NRF_SUCCESS) {
         printf("Error initializing nrf backend\n");
@@ -281,16 +181,5 @@ int main(void)
     }
     nrf_sdk_sha256();
     nrf_sdk_aes();
-#elif ARM_CRYPTOCELL
-    cryptocell_setup();
-    sha1_test();
-    sha256_test();
-    aes_test();
-    cryptocell_terminate();
-#else
-    sha1_test();
-    sha256_test();
-    aes_test();
-#endif
     return 0;
 }
