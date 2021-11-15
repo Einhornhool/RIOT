3b0cb18635ffdde145576a9437e447636e3247f3
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2021-11-15 18:34:58 +0100

    Add ECC stuff

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -128,6 +128,7 @@ PSEUDOMODULES += prng
 PSEUDOMODULES += prng_%
 PSEUDOMODULES += fortuna_reseed
 PSEUDOMODULES += psa_arch_tests
+PSEUDOMODULES += psa_uecc_%
 PSEUDOMODULES += psa_riot_cipher_%
 PSEUDOMODULES += psa_riot_hashes_%
 PSEUDOMODULES += psa_tinycrypt_%
