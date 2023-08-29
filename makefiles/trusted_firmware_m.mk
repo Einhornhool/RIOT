8d78f7d41a33051c61a341ffbf5958801f969ccd
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2023-08-29 17:25:12 +0200

    makefiles: update tfm makefile

--- makefiles/trusted_firmware_m.mk
+++ makefiles/trusted_firmware_m.mk
@@ -11,20 +11,35 @@ MERGED_BINARY = $(BINDIR)/s_ns_merged_signed.bin
 
 IMAGE_SIGNING_DIR = $(RIOTTOOLS)/trusted_firmware_m/image_signing
 IMGTOOL_WRAPPER = $(IMAGE_SIGNING_DIR)/scripts/wrapper/wrapper.py
+IMGTOOL ?= $(RIOTTOOLS)/mcuboot/imgtool.py
 SIGNING_LAYOUT_S = $(IMAGE_SIGNING_DIR)/layout_files/signing_layout_s.o
 SIGNING_LAYOUT_NS = $(IMAGE_SIGNING_DIR)/layout_files/signing_layout_ns.o
 
 SIGNING_KEYFILE_S = $(PKG_SOURCE_DIR)/bl2/ext/mcuboot/root-RSA-3072.pem
 SIGNING_KEYFILE_NS = $(PKG_SOURCE_DIR)/bl2/ext/mcuboot/root-RSA-3072_1.pem
+# SIGNING_KEYFILE = $(BINDIR)/key.pem
 
 ASSEMBLE_IMAGES = $(IMAGE_SIGNING_DIR)/scripts/assemble.py
+
+# $(info $$**************Includes is [${INCLUDES}])
+# $(info $$**************objects is [${_LINK}])
+# $(info $$**************link is [${LINK}])
+# $(info $$**************baselibs is [${BASELIBS}])
+# $(info $$**************archives is [${ARCHIVES}])
+# tfm-create-key: $(SIGNING_KEYFILE)
+
+# ifeq ($(BINDIR)/key.pem,$(SIGNING_KEYFILE))
+# $(SIGNING_KEYFILE):
+# 	$(Q)mkdir -p $(BINDIR)
+# 	$(Q)$(IMGTOOL) keygen -k $@ -t rsa-2048
+# endif
 
 # 1. Re-link files with new offset
 # 2. Sign secure and non-secure image and merge them into one binary
 # This uses the scripts located at dist/tools/trusted_firmware_m.
 trusted_firmware_m: ROM_OFFSET=$$(($(MCUBOOT_SLOT0_SIZE) + $(IMAGE_HDR_SIZE)))
-trusted_firmware_m: link
-	$(Q)$(_LINK) -o $(ELFFILE) && \
+trusted_firmware_m: link # tfm-create-key
+	$(Q) $(_LINK) -o $(ELFFILE) && \
 	$(OBJCOPY) $(OFLAGS) -Obinary $(ELFFILE) $(BINFILE) && \
 	$(IMGTOOL_WRAPPER) -v 1.7.0 --layout $(SIGNING_LAYOUT_S) -k $(SIGNING_KEYFILE_S) \
 	--public-key-format full --align 1 --pad --pad-header -H $(IMAGE_HDR_SIZE) \
