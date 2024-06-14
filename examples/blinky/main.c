af00ee6b88ee2f1e6005a159bee7cca1bc487b33
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2024-06-14 14:39:45 +0200

    examples/blinky: Update blinky to use ns entry functions

--- examples/blinky/main.c
+++ examples/blinky/main.c
@@ -1,61 +1,41 @@
 /*
- * Copyright (C) 2021 Otto-von-Guericke-Universität Magdeburg
+ * Copyright (C) 2024 HAW Hamburg
  *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
+ * This file is subject to the terms and conditions of the GNU GENERAL PUBLIC LICENSE.
+ * See the file LICENSE in the top level directory for more details.
+ *
+ * The code below is based on the code here: https://devzone.nordicsemi.com/f/nordic-q-a/92877/bare-metal-programming-on-the-nrf9160
  */
 
 /**
- * @ingroup     examples
- * @{
+ * @file        main.c
+ * @brief
  *
- * @file
- * @brief       Blinky application
- *
- * @author      Marian Buschsieweke <marian.buschsieweke@ovgu.de>
- *
- * @}
+ * @author      Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
  */
 
 #include <stdio.h>
-
-#include "clk.h"
-#include "board.h"
-#include "periph_conf.h"
-#include "timex.h"
-#include "ztimer.h"
-
-static void delay(void)
-{
-    if (IS_USED(MODULE_ZTIMER)) {
-        ztimer_sleep(ZTIMER_USEC, 1 * US_PER_SEC);
-    }
-    else {
-        /*
-         * As fallback for freshly ported boards with no timer drivers written
-         * yet, we just use the CPU to delay execution and assume that roughly
-         * 20 CPU cycles are spend per loop iteration.
-         *
-         * Note that the volatile qualifier disables compiler optimizations for
-         * all accesses to the counter variable. Without volatile, modern
-         * compilers would detect that the loop is only wasting CPU cycles and
-         * optimize it out - but here the wasting of CPU cycles is desired.
-         */
-        uint32_t loops = coreclk() / 20;
-        for (volatile uint32_t i = 0; i < loops; i++) { }
-    }
-}
+#include "nrfx.h"
+#include "secure_led.h"
 
 int main(void)
 {
-    while (1) {
-        delay();
-#ifdef LED0_TOGGLE
-        LED0_TOGGLE;
-#else
-        puts("Blink! (No LED present or configured...)");
-#endif
+    puts("Wheeee \\o/");
+    config_leds();
+    while (1)
+    {
+        // In the main loop, we make each of the LEDs light up one at a time
+        for (uint32_t i = 0; i < 4; ++i)
+        {
+            led_on(i);
+
+            // Some delay
+            volatile uint32_t j = 1000000;
+            while (j > 0)
+                --j;
+
+            led_off(i);
+        }
     }
 
     return 0;
